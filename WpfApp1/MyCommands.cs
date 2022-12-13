75150927c945c8a58b7e37dbc52a94ce6dc887fd
DmEnd <Dima_polit-sov@mail.ru>
2022-12-13 03:48:48 +0300

    Добавлены привязки данных.

---
+++ WpfApp1/MyCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    internal class MyCommands
+    {
+        //Объявляем команду
+        public static RoutedCommand Exit { get; set; }
+        //Инициализируем команду
+        static MyCommands()
+        {
+            Exit = new RoutedCommand();
+        }
+    }
+}
